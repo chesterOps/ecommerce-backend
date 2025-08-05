6ab6703599cc3890b41254c2661a77c1bb1181ca
Okwulehie Tochukwu <ochester4@gmail.com>
2025-08-05 15:15:57 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import dotenv from "dotenv";
+
+// Configure env
+dotenv.config();
+
+import cookieParser from "cookie-parser";
+
+import express, { Express, Response, Request, NextFunction } from "express";
+
+// Create express app
+const app: Express = express();
+
+// Define port
+const PORT: number = Number(process.env.PORT || 3000);
+
+// Parse cookie
+app.use(cookieParser());
+
+// Parse body
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
+
+// Send response
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Hello there",
+  });
+});
+
+// Start server
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Uncaught exception
+process.on("uncaughtException", (err: Error) => {
+  console.log("Uncaught exception!, Shutting down...");
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
+// Unhandled rejection
+process.on("unhandledRejection", (err: Error) => {
+  console.log("Unhandled rejection!, Shutting down...");
+  console.error(err.name, err.message);
+  server.close(() => process.exit(1));
+});
---
+++ src/middlewares/filter.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+
+export default function filter(body: Array<string>) {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    // New body
+    const newBody: { [key: string]: any } = {};
+
+    // Filter fields from request body
+    Object.keys(req.body).forEach((el) => {
+      if (body.includes(el)) newBody[el] = req.body[el];
+    });
+
+    // Update request body
+    req.body = newBody;
+
+    // Next middleware
+    next();
+  };
+}
